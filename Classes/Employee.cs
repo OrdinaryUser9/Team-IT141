bdfcc8709ee9dd94ac03ce5b281ece54f493ea3b
mikayilovelshan <elshanmikayilov004@gmail.com>
2026-06-06 18:48:34 +0400

    elshan task hazirdir

--- Classes/Employee.cs
+++ Classes/Employee.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleProject.Classes;
 
-public class Employee
+public class Employee: Department
 {
+    public string Name { get; set; }    
+
+    public string FirstName { get; set; }   
+
+    public int Age { get; set; }
+
+    public double Salary { get; set; }
+
+    public double BonusPay()
+    {
+
+        return Salary + (Salary * 20 / 100);
+    }
 }
