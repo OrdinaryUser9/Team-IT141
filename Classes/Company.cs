f2d07b81fc5a5b70f16cf77d7be393f9f22e28d7
OrdinaryUser9 <nijatrustamli2005@gmail.com>
2026-06-06 18:24:13 +0400

    First Commit

---
+++ Classes/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject.Classes;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string LogoPng { get; set; }
+    public int Budget { get; set; }
+
+    public void BudceniHesabla()
+    {
+        Console.WriteLine("Budce: " + Budget);
+    }
+
+}
